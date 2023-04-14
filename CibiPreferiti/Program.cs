1418aa503b9dba032069796b0db2901afff0974c
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-14 16:04:34 +0200

    First exercise, part of bonus exercise

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿string[] cibiPreferiti = { "Patatine fritte", "Carne", "Gelato", "Formaggio", "Verdura", "Frutta" };
+
+Console.WriteLine(findArrayLength(cibiPreferiti));
+Console.WriteLine();
+classificaArray(cibiPreferiti);
+Console.WriteLine();
+Console.WriteLine(firstElementInArray(cibiPreferiti));
+Console.WriteLine();
+Console.WriteLine(lastElementInArray(cibiPreferiti));
+
+
+
+
+
+
+//----------------- FUNZIONI -------------------
+
+//Funzione per trovare la lunghezza di un array
+
+int findArrayLength(string[] array)
+{
+	int arrayLength = array.Length;
+	return arrayLength;
+};
+
+//Classifica Array
+
+void classificaArray(string[] array) 
+{
+	for(int i = 0; i < array.Length; i++)
+	{
+		Console.WriteLine((i + 1) + "° - " + array[i]);
+	}
+}
+
+//Primo elemento di un array
+
+string firstElementInArray(string[] array)
+{
+	string firstElement = array[0];
+	return firstElement;
+}
+
+//Ultimo elemento di un array
+string lastElementInArray(string[] array)
+{
+	string lastElement = array[array.Length - 1];
+	return lastElement;
+}
+
---
+++ CibiPreferitiBonus/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Quanti cibi vuoi inserire? ");
+string[] userCibiPreferiti = new string[int.Parse(Console.ReadLine())];
+
+for(int i = 0; i < userCibiPreferiti.Length; i++)
+{
+	Console.Write("Cosa vuoi inserire come " + (i + 1) + "° cibo? ");
+	string userCibi = Console.ReadLine();
+	userCibiPreferiti[i] = userCibi;
+}
+
+
+
+
+//----------FUNZIONI----------------
+
+
